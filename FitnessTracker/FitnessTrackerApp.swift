363d087f7bacfb7afeb442649fd947382cc241a1
bukayooo <ifeoluwaodedele@gmail.com>
2025-02-25 16:14:49 -0500

    Initial Commit

---
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker
+//
+//  Created by Bukayo Odedele on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessTrackerTests/FitnessTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerTests.swift
+//  FitnessTrackerTests
+//
+//  Created by Bukayo Odedele on 2/25/25.
+//
+
+import Testing
+@testable import FitnessTracker
+
+struct FitnessTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
